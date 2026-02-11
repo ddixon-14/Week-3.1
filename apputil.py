74e39863aefb4078358b0e8ced2695a1beb2b864
Danielle D. <dani.dixonpro@gmail.com>
2026-02-10 23:22:04 -0500

    addition of fib(n) from exercises

--- apputil.py
+++ apputil.py
@@ -2,4 +2,15 @@ import seaborn as sns
 import pandas as pd
 
 
-# update/add code below ...
+"""Fib(n) returns the n'th number of the Fibonacci series"""
+def fib(n):
+    if n == 0: #base case
+        return 0
+    elif n == 1: #base case
+        return 1
+    else:
+        return fib(n-1) + fib(n-2) #recursive case
+    
+print(fib(10)) 
+
+
